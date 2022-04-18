ead3aa49eb63d412076d3563181a9e85065e8d9b
Jhon Perez <perezl.40nlsa.teleperformance.com>
2022-04-18 11:44:32 -0500

    probando desde pc de la empresa

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,29 +1,31 @@
 const fizzbuzz = () => {
   for (let i = 1; i <= 100; i++) {
-      if (i%3 === 0 && i%5 ===0) {
-          console.log('FizzBuzz')
-      } else if (i%3 === 0 ) {
-        console.log('Fizz')
-      }else if (i%5 ===0){
-        console.log('Buzz')
-      }else{
-          console.log(i)
-      }
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz')
+    } else if (i % 3 === 0) {
+      console.log('Fizz')
+    } else if (i % 5 === 0) {
+      console.log('Buzz')
+    } else {
+      console.log(i)
+    }
   }
-};
+}
+// fizzbuzz()
+// fizzbuzz()
 // fizzbuzz()
 
 const fizzbuzz2 = () => {
-    for (let i = 1; i <= 100; i++) {
-        let log = ''
-        if (i%3 === 0 ) {
-            log += 'Fizz'
-        }
-        if (i%5 === 0 ) {
-            log += 'Buzz'
-        }
-        console.log(log || i)
+  for (let i = 1; i <= 100; i++) {
+    let log = ''
+    if (i % 3 === 0) {
+      log += 'Fizz'
     }
-};
+    if (i % 5 === 0) {
+      log += 'Buzz'
+    }
+    console.log(log || i)
+  }
+}
 
 fizzbuzz2()
